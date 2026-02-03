d29cb7a772732f3009bdc7f5773793dd4e64d235
Douglas <douglasp.r.desouza@gmail.com>
2026-02-02 22:06:42 -0300

    Autenticação e Login Social - Desenvolvendo a Landing page

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { MatCardModule } from '@angular/material/card'
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { MatButtonModule } from '@angular/material/button'
 import { Profile } from './profile.model';
 
 
 @Component({
   selector: 'app-landingpage',
-  imports: [],
+  imports: [ MatCardModule, MatButtonModule, FlexLayoutModule ],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.scss'
 })
