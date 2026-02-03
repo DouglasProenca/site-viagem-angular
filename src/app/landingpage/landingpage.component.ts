4609fa90ccb6e7a43687d9d52f507057a9d578f6
Douglas <douglasp.r.desouza@gmail.com>
2026-02-02 21:01:47 -0300

    Autenticação e Login Social - criando a página inicial da aplicação

---
+++ src/app/landingpage/landingpage.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Profile } from './profile.model';
+
+
+@Component({
+  selector: 'app-landingpage',
+  imports: [],
+  templateUrl: './landingpage.component.html',
+  styleUrl: './landingpage.component.scss'
+})
+export class LandingpageComponent {
+
+  profile: Profile | undefined;
+
+  constructor(private router: Router){
+
+  }
+
+  navegar(){
+    this.router.navigate(['/paginas/galeria']);
+  }
+
+  logarComGoogle(){
+
+  }
+
+  isLoggedIn(){
+    return !!this.profile;
+  }
+}
